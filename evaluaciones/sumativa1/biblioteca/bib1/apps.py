58d2bd428c82ffabb19c84323873ffcb7c31cfd1
Victor Bonta B. <victor.bonta@inacapmail.cl>
2024-09-21 00:41:48 -0300

    .

--- evaluaciones/sumativa1/biblioteca/bib1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Bib1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bib1'
--- evaluaciones/sumativa1/biblioteca/bib1/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def productos_view(request):
-    return render(request, 'productos.html')
