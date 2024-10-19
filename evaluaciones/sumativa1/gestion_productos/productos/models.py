9c390b5ddd7da731e52636cbaa98a1f1d4faa8c9
Victor Bonta B. <victor.bonta@inacapmail.cl>
2024-10-18 22:08:10 -0300

    ..

--- evaluaciones/sumativa1/gestion_productos/productos/models.py
+++ evaluaciones/sumativa1/gestion_productos/productos/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Caracteristica(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    codigo = models.CharField(max_length=50, unique=True)  # Código único
+    nombre = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, blank=True)
+    caracteristicas = models.ManyToManyField(Caracteristica, blank=True)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.codigo})"
