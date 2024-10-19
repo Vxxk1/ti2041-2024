c1ef6d4fd48cc1379f3407812b2a7277f3b6749e
Victor Bonta B. <victor.bonta@inacapmail.cl>
2024-10-18 22:14:57 -0300

    .

--- evaluaciones/sumativa1/gestion_productos/productos/admin.py
+++ evaluaciones/sumativa1/gestion_productos/productos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Marca, Categoria, Caracteristica, Producto
+
+# Registro de los modelos
+admin.site.register(Marca)
+admin.site.register(Categoria)
+admin.site.register(Caracteristica)
+admin.site.register(Producto)
